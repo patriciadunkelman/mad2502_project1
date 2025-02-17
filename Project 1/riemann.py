752608e0521750fd8e2875a492b52b737292488c
patriciadunkelman <paddylin2005@gmail.com>
2025-02-17 16:38:47 -0500

    empty assignment

---
+++ Project 1/riemann.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+def left_endpoint(x_vals: np.array, func: np.ufunc):
+    return 1
+
+def trapezoid(x_vals: np.array, func: np.ufunc):
+    return 1
+
+def simpson(x_vals: np.array, func: np.ufunc):
+    return 1
