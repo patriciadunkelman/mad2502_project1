7ad4fbaf8f1d5fc897298b405a53b5d2a7c59bce
patriciadunkelman <paddylin2005@gmail.com>
2025-02-17 17:09:54 -0500

    empty assignment

---
+++ Project 1/barycentric.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+def get_barycentric_coordinates(triangle_coordinates: np.array, point_coordinates: np.array) -> np.array:
+    return np.array()
+
+def get_cartesian_coordinates(triangle_coordinates: np.array, barycentric_coordinates: np.array) -> np.array:
+    return np.array()
+
+def is_inside_triangle(triangle_coordinates: np.array, point_coordinates: np.array) -> bool:
+    return True
