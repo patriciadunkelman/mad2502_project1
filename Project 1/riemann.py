bbe69d633301c239d60472dbe169cbfb66ee29ae
averyreynolds <averyreynolds@ufl.edu>
2025-02-20 10:44:16 -0500

    finished left endpoint function

--- Project 1/riemann.py
+++ Project 1/riemann.py
@@ -1,10 +1,16 @@
 import numpy as np
 
 def left_endpoint(x_vals: np.array, func: np.ufunc):
-    return 1
+    approx = 0
+    n = len(x_vals) - 1
+    for i in range(n):
+        area = func(x_vals[i]) * (x_vals[i + 1] - x_vals[i])
+        approx += area
+    return approx
 
 def trapezoid(x_vals: np.array, func: np.ufunc):
     return 1
 
 def simpson(x_vals: np.array, func: np.ufunc):
     return 1
+
